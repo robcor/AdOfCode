e4af34c1c3650d1ffafce9ce8585c74e6a920a45
rcorda <rcorda@pippo.pippo>
2020-12-20 15:33:30 +0100

    D3 test disabled

--- src/test/java/TestDay03.java
+++ src/test/java/TestDay03.java
@@ -1,37 +1,36 @@
-import org.corda.Quiz0201Resolver;
-import org.corda.Quiz0202Resolver;
 import org.corda.Quiz0301Resolver;
 import org.corda.Quiz0302Resolver;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
- class TestDay03 {
+class TestDay03 {
 
-     @Test
-     void firstQuizRefactoring() throws Exception {
+    @Disabled
+    @Test
+    void firstQuizRefactoring() throws Exception {
 
-         Quiz0301Resolver resolver = new Quiz0301Resolver( "daythree.txt" );
+        Quiz0301Resolver resolver = new Quiz0301Resolver( "daythree.txt" );
 
-         long result = resolver.resolve();
+        long result = resolver.resolve();
 
-         assertEquals( 42, result );
+        assertEquals( 42, result );
 
-         System.out.println( "D3Q1 result: " + result );
-     }
+        System.out.println( "D3Q1 result: " + result );
+    }
 
-     @Test
-     void secondQuizRefactoring() throws Exception {
+    @Disabled
+    @Test
+    void secondQuizRefactoring() throws Exception {
 
-         Quiz0302Resolver resolver = new Quiz0302Resolver( "daythree.txt" );
+        Quiz0302Resolver resolver = new Quiz0302Resolver( "daythree.txt" );
 
-         long result = resolver.resolve();
+        long result = resolver.resolve();
 
-         assertEquals( 42, result );
+        assertEquals( 42, result );
 
-         System.out.println( "D3Q2 result: " + result );
-     }
-
-
+        System.out.println( "D3Q2 result: " + result );
+    }
 
 
 }
